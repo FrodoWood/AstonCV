109af728bdacba8acd2defd82558a30c92451957
FrodoWood <elijahbaggins3@gmai.com>
2022-04-09 19:03:28 +0100

    Added edit page for cv, and styled edit form

---
+++ resources/views/cvs/edit.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+
+<div class="container mt-4">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header text-capitalize">{{ $cv->name }}</div>
+
+
+                <form class="d-flex flex-column mx-auto py-4" method="POST" action="/cvs/{{ $cv->id }}">
+                    @method('PUT')
+                    @csrf
+
+                    <div class="row">
+                        <label class="col-4" for="name">Name</label>
+                        <input class="col-8 mb-3" type="text" id="name" name="name">
+                    </div>
+
+                    <div class="row">
+                        <label class="col-4" for="email">Email</label>
+                        <input class="col-8 mb-3" type="email" id="email" name="email">
+                    </div>
+
+                </form>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
