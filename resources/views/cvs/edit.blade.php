bfc30d9e2f1ae322f756d55ee2785bce879244ef
FrodoWood <elijahbaggins3@gmai.com>
2022-04-17 03:10:05 +0100

    Fixed update/delete issue. Website is now fully working as expected after tesing all functionalities as guest user and logged in user.

--- resources/views/cvs/edit.blade.php
+++ resources/views/cvs/edit.blade.php
@@ -11,7 +11,7 @@
                 <div class="card-header text-capitalize">{{ $cv->name }}</div>
 
 
-                <form class="d-flex flex-column  py-4" method="POST" action="/cvs/{{ $cv->id }}">
+                <form id="update-form" class="d-flex flex-column  py-4" method="POST" action="/cvs/{{ $cv->id }}">
                     @method('PUT')
                     @csrf
 
@@ -62,10 +62,10 @@
                         <textarea name="URLlinks" id="URLlinks" cols="30" rows="3" class="col-6 mb-3">{{ $cv->URLlinks }}</textarea>
                     </div>
 
-                    <div class="row px-2 justify-content-center">
-
+                    <!-- <div class="row px-2 justify-content-center">
                         <div class="col-4">
                             <button type="submit" class="btn btn-success">Update</button>
+
                             <a class="btn btn-secondary" href="{{route('cvs')}}" role="button">Cancel</a>
                         </div>
                         <div class="col-6">
@@ -79,10 +79,36 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
+
+                    <!-- <button type="submit" class="btn btn-success">Update</button> -->
 
                 </form>
 
+                <!-- <form method="POST" action="/cvs/{{ $cv->id }}">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form> -->
+
+                <div class="row px-2 pb-4 justify-content-center">
+                    <div class="col-4">
+                        <button form="update-form" type="submit" class="btn btn-success">Update</button>
+
+                        <a class="btn btn-secondary" href="{{route('cvs')}}" role="button">Cancel</a>
+                    </div>
+                    <div class="col-6">
+                        <div class="row justify-content-end">
+                            <div class="col-4">
+                                <form method="POST" action="/cvs/{{ $cv->id }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
